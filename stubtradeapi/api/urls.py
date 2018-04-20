a7c7872f144fe2387772b52ea93bc38edae29d92
Nitigya Sharma <nitigyasharma@hotmail.com>
2018-04-20 23:52:40 +0530

    Allow to fetch data based on range

--- stubtradeapi/api/urls.py
+++ stubtradeapi/api/urls.py
@@ -3,11 +3,15 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import ListView
 from .views import CreateView
 from .views import SymbolView
+from .views import ExactSymbolView
+from rest_framework.documentation import include_docs_urls
 
 
 urlpatterns = {
+    url(r'^docs/', include_docs_urls(title='StubTradeAPI Documentation')),
     url(regex=r'^ticklist/all/$' ,view=ListView.as_view()),
     url(regex=r'^ticklist/(?P<symbol>\w{1,20})/(?P<range>\d+\w+)/$', view=SymbolView.as_view()),
+    url(regex=r'^ticklist/(?P<symbol>\w{1,20})/exact/(?P<range>\d+\w+)/$', view=ExactSymbolView.as_view()),
     url(regex=r'tickadd/$' ,view=CreateView.as_view()),
 }
 
